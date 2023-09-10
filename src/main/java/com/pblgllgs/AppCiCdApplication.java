b520baef716e086163181d867b832143aec4a75a
pbl.gllgs <pbl.gllgs@gmail.com>
2023-09-10 16:02:56 -0300

    test CI/CD

--- src/main/java/com/pblgllgs/AppCiCdApplication.java
+++ src/main/java/com/pblgllgs/AppCiCdApplication.java
@@ -1,13 +1,24 @@
 package com.pblgllgs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppCiCdApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppCiCdApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(AppCiCdApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args -> {
+			LOGGER.info("App run successfully");
+		};
+	}
 }
