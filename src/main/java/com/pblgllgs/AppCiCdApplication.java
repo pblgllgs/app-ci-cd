e22498d16938a5a3ea5ae7fbcb89c3910cc34475
pbl.gllgs <pbl.gllgs@gmail.com>
2023-09-10 16:00:22 -0300

    test CI/CD

---
+++ src/main/java/com/pblgllgs/AppCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.pblgllgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCiCdApplication.class, args);
+	}
+
+}
