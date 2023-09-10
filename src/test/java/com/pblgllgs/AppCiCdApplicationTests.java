0ca0e287edb791a59af2ba8261f6ba24bed2b54b
pbl.gllgs <pbl.gllgs@gmail.com>
2023-09-10 17:29:29 -0300

    add content in test

--- src/test/java/com/pblgllgs/AppCiCdApplicationTests.java
+++ src/test/java/com/pblgllgs/AppCiCdApplicationTests.java
@@ -1,13 +1,22 @@
 package com.pblgllgs;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AppCiCdApplicationTests {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppCiCdApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		String msg = "Tests run successfully";
+		LOGGER.info(msg);
+		assertThat(msg).isEqualTo("Tests run successfully");
 	}
 
 }
