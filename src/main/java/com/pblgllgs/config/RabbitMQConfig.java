6c4c561a760b57bed024018f7da8c3854fc14e45
pbl.gllgs <pbl.gllgs@gmail.com>
2023-11-18 10:23:59 -0300

    env variables

--- src/main/java/com/pblgllgs/config/RabbitMQConfig.java
+++ src/main/java/com/pblgllgs/config/RabbitMQConfig.java
@@ -13,32 +13,39 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    private static final String EXCHANGE_NAME = "ex_logstash";
-    private static final String ROUTING_KEY = "logs";
+    @Value("${broker.name.key}")
+    private String routingKey;
+
+    @Value("${broker.name.exchange}")
+    private String exchange;
+
+    @Value("${broker.name.queue}")
+    private String queue;
 
     @Bean
     public Exchange logsExchange() {
-        return ExchangeBuilder.directExchange(EXCHANGE_NAME).durable(true).build();
+        return ExchangeBuilder.directExchange(exchange).durable(true).build();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,Jackson2JsonMessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setExchange(EXCHANGE_NAME);
-        rabbitTemplate.setRoutingKey(ROUTING_KEY);
+        rabbitTemplate.setExchange(exchange);
+        rabbitTemplate.setRoutingKey(routingKey);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
 
     @Bean
     public Queue myQueue() {
-        return new Queue("queue-spring", true);
+        return new Queue(queue, true);
     }
 
     @Bean
