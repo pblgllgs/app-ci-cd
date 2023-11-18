29ed4d6b05607e10aeb3c9d741a03a73dc9d80b7
pbl.gllgs <pbl.gllgs@gmail.com>
2023-11-18 13:59:18 -0300

    delete action, add json logs

--- src/main/java/com/pblgllgs/controller/Controller.java
+++ src/main/java/com/pblgllgs/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.pblgllgs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class Controller {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+
     @GetMapping
     public String hello(){
+        LOGGER.info("HELLO!!");
         return "Hello!!";
     }
 }
