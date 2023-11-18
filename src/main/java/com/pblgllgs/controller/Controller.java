3fb0a98c7cb4ef477ad1a966e0c51e074c17e553
pbl.gllgs <pbl.gllgs@gmail.com>
2023-11-18 14:05:52 -0300

    remove rabbitmq, move to v4 and add logs in format json

--- src/main/java/com/pblgllgs/controller/Controller.java
+++ src/main/java/com/pblgllgs/controller/Controller.java
@@ -1,29 +1,19 @@
 package com.pblgllgs.controller;
 
-import com.pblgllgs.Message;
-import com.pblgllgs.services.LogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/hello")
 @Slf4j
 @RequiredArgsConstructor
 public class Controller {
-    private final LogService logService;
 
     @GetMapping
     public void hello() {
-        Message message =  Message.builder()
-                .id(UUID.randomUUID().toString())
-                .message("<----------------Hello from logs!!!--------------->" + Instant.now().toString())
-                .build();
-        logService.sendLogMessage(message);
+        log.info("HELLO");
     }
 }
