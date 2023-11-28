d328ff41c33c1bba4f3ce0c8bfa8bff92dcf44ef
pbl.gllgs <pbl.gllgs@gmail.com>
2023-11-28 17:58:41 -0300

    add logs message

--- src/main/java/com/pblgllgs/controller/Controller.java
+++ src/main/java/com/pblgllgs/controller/Controller.java
@@ -1,15 +1,18 @@
 package com.pblgllgs.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class Controller {
 
     @GetMapping
     public String hello(){
+        log.info("Hello!!");
         return "Hello!!";
     }
 }
